0868030c92cf3fcdea27902874130d4e26089286
Mateusz Kocz <mateusz@kocz.pl>
2017-02-13 18:59:35 +0100

    tests(lib): add placeholders tests

--- src/color-selector.component.spec.ts
+++ src/color-selector.component.spec.ts
@@ -1,5 +1,5 @@
 import {TestBed, ComponentFixture, async} from '@angular/core/testing'
-import {DebugElement} from '@angular/core'
+import {Component, DebugElement} from '@angular/core'
 import {By} from '@angular/platform-browser'
 import {ColorSelectorComponent} from './color-selector.component'
 import {ColorSelectorModule} from './color-selector.module'
@@ -26,5 +26,34 @@ describe('Color selector component', () => {
   it('shoud render in the DOM', () => {
     // TODO
     expect(debugElement.query(By.css('div')).nativeElement.textContent).toBe('Color Selector')
+  })
+
+  it('should display colors provided from the parent component', () => {
+
+  })
+
+  it('should limit the amount of displayed colors if the limit is provided', () => {
+
+  })
+
+  it('should display empty, non-clickable items when the padding value is provided', () => {
+
+  })
+
+  it('should inform the parent component about color selection', () => {
+
+  })
+
+  it('should optionally display an indication of what color is selected', () => {
+
+  })
+
+  it('should filter falsy or non-color values', () => {
+
+  })
+
+  // TODO: Decide
+  it('should optionally display the color\'s name', () => {
+
   })
 })
